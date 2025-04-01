6ed98115bfdf654dbd49a46d837e0372758c6c04
Elena Cristescu <elenacristescu11@gmail.com>
2025-04-01 17:06:37 +0200

    commit

--- Everleaf.Model/obj/Debug/net9.0/Everleaf.Model.AssemblyInfo.cs
+++ Everleaf.Model/obj/Debug/net9.0/Everleaf.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Everleaf.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d31f57ad1f3f04ad6b3c281b84aa7c9414d92ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c9b6b62daff4bdea396b1dd3201889754831e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Everleaf.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Everleaf.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
