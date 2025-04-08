f7829dea4a0b0e901e48505a7eb120ac0d55ea06
Elena Cristescu <elenacristescu11@gmail.com>
2025-04-08 16:39:09 +0200

    Add PlantDTO class to define plant data structure and properties

---
+++ Everleaf.Model/DTOs/PlantDTO.cs
@@ -0,0 +1,13 @@
+namespace Everleaf.Model.DTOs
+{
+    public class PlantDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Nickname { get; set; }
+        public int Species { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int UserId { get; set; }
+    }
+}
