02e8ba3f1d05141414ced2d1835f8c4f9b8a08bf
Madalina02 <72490950+Mada201201@users.noreply.github.com>
2025-05-10 17:32:46 +0200

    plant service unit test

--- client/src/app/Services/plant.service.spec.ts
+++ client/src/app/Services/plant.service.spec.ts
@@ -1,16 +1,72 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
 import { PlantService } from './plant.service';
 
-describe('PlantService', () => {
+describe('PlantService (minimal)', () => {
   let service: PlantService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        PlantService
+      ]
+    });
     service = TestBed.inject(PlantService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getPlants() returns whatever http.get provides', (done) => {
+    service['http'] = {
+      get: () => ({
+        pipe: () => ({
+          subscribe: (next: any) => next([{ id: 42, name: 'MyPlant' }])
+        })
+      })
+    } as any;
+
+    service.getPlants().subscribe(plants => {
+      expect(plants.length).toBe(1);
+      expect(plants[0].name).toBe('MyPlant');
+      done();
+    });
+  });
+
+  it('getPlants() propagates http errors', (done) => {
+    service['http'] = {
+      get: () => ({
+        pipe: () => ({
+          subscribe: (observer: any) => observer.error('oops')
+        })
+      })
+    } as any;
+
+    service.getPlants().subscribe({
+      next: () => fail('should not succeed'),
+      error: e => {
+        expect(e).toBe('oops');
+        done();
+      }
+    });
+  });
+
+  it('savePlant() returns whatever http.post provides', (done) => {
+    const newPlant = { nickname: 'New' };
+    service['http'] = {
+      post: (_url: string, body: any) => ({
+        pipe: () => ({
+          subscribe: (next: any) => next({ ok: true, sent: body })
+        })
+      })
+    } as any;
+
+    service.savePlant(newPlant).subscribe(resp => {
+      expect(resp.ok).toBeTrue();
+      expect(resp.sent.nickname).toBe('New');
+      done();
+    });
   });
 });
