34e962a855dd3016aae4a9613e916f320a6f461a
Isabel <132066834+isabel21201@users.noreply.github.com>
2025-05-12 14:29:25 +0200

    Review: Backend API Controllers & Core Data Structures

--- Everleaf.Model/Entities/Plant.cs
+++ Everleaf.Model/Entities/Plant.cs
@@ -3,11 +3,11 @@ using System;
 public class Plant
 {
     public Plant() {} 
-    public int Id { get; set; } 
-    public string? Name { get; set; }
+    public int Id { get; set; } // Unique identifier for the plant
+    public string? Name { get; set; } 
     public string? Nickname { get; set; }
     public int? Species { get; set; }  // FK to PlantType - Changed to nullable int
     public string? ImageUrl { get; set; }
-    public DateTime DateAdded { get; set; }
+    public DateTime DateAdded { get; set; } //Date the plant was added 
     public int UserId { get; set; }  // FK to User
 }
