96f9d44e1d69d9fa01222e6adf08f6fcf53621c2
Isabel <132066834+isabel21201@users.noreply.github.com>
2025-05-12 14:40:41 +0200

    Accepting Plant.cs changes

--- Everleaf.Model/Entities/Plant.cs
+++ Everleaf.Model/Entities/Plant.cs
@@ -6,13 +6,8 @@ public class Plant
     public int Id { get; set; } // Unique identifier for the plant
     public string? Name { get; set; } 
     public string? Nickname { get; set; }
-<<<<<<< HEAD
     public int? Species { get; set; }  // FK to PlantType - Changed to nullable int
     public string? ImageUrl { get; set; }
     public DateTime DateAdded { get; set; } //Date the plant was added 
-=======
-    public int Species { get; set; }  // FK to PlantType
-    public DateTime DateAdded { get; set; }
->>>>>>> daac8346546105ac2e0ffd454ff697be02162e46
     public int UserId { get; set; }  // FK to User
 }
